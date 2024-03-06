0a4c6cdd8d847ae6c9f844c69b0c030d820e0401
Jordan Lee <jordan@jordanlee.net>
2024-03-06 22:21:41 +1100

    update profile component to list claims

--- spa/src/app/profile/profile.component.ts
+++ spa/src/app/profile/profile.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
 
+export class ProfileComponent implements OnInit {
+  displayedColumns: string[] = ['claim', 'value'];
+  dataSource: Claim[] = [];
+  private readonly _destroying$ = new Subject<void>();
+  
+  constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
+
+  ngOnInit(): void {
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) =>  status === InteractionStatus.None || status === InteractionStatus.HandleRedirect),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.checkAndSetActiveAccount();
+        this.getClaims(this.authService.instance.getActiveAccount()?.idTokenClaims)
+      })
+  }
+
+  checkAndSetActiveAccount() {
+
+    let activeAccount = this.authService.instance.getActiveAccount();
+
+    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
+      let accounts = this.authService.instance.getAllAccounts();
+      this.authService.instance.setActiveAccount(accounts[0]);
+    }
+  }
+
+  getClaims(claims: any) {
+
+    let list: Claim[]  =  new Array<Claim>();
+
+    Object.keys(claims).forEach(function(k, v){
+      
+      let c = new Claim()
+      c.id = v;
+      c.claim = k;
+      c.value =  claims ? claims[k]: null;
+      list.push(c);
+    });
+    this.dataSource = list;
+
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
+}
+
+export class Claim {
+  id: number;
+  claim: string;
+  value: string;
 }
