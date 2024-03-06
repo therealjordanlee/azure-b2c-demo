a885a54bd1b753dd0a1f1f37194a9ad04f1f05d4
Jordan Lee <jordan@jordanlee.net>
2024-03-06 14:39:41 +1100

    initial spa skeleton

---
+++ spa/src/app/webapi/webapi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webapi',
+  templateUrl: './webapi.component.html',
+  styleUrl: './webapi.component.css'
+})
+export class WebapiComponent {
+
+}
