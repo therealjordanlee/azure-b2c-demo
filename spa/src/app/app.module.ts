99fbccad35bf883bb7fe6db3bee700a9aa2a1070
Jordan Lee <jordan@jordanlee.net>
2024-03-06 22:12:24 +1100

    add msal configuration to app modules

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+/* Changes start here. */
+// Import MSAL and MSAL browser libraries. 
+import { MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
+// Import the Azure AD B2C configuration 
+import { msalConfig, protectedResources } from './auth-config';
+
+// Import the Angular HTTP interceptor. 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,9 +28,42 @@ import { WebapiComponent } from './webapi/webapi.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Import the HTTP client. 
+    HttpClientModule,
+    // Initiate the MSAL library with the MSAL configuration object
+    MsalModule.forRoot(new PublicClientApplication(msalConfig),
+      {
+        // The routing guard configuration. 
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: protectedResources.todoListApi.scopes
+        }
+      },
+      {
+        // MSAL interceptor configuration.
+        // The protected resource mapping maps your web API with the corresponding app scopes. If your code needs to call another web API, add the URI mapping here.
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map([
+          [protectedResources.todoListApi.endpoint, protectedResources.todoListApi.scopes]
+        ])
+      })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    /* Changes start here. */
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalGuard
+    /* Changes end here. */
+  ],
+  bootstrap: [
+    AppComponent,
+    /* Changes start here. */
+    MsalRedirectComponent
+    /* Changes end here. */
+  ]
 })
 export class AppModule { }
