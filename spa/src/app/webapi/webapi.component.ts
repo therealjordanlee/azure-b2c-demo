29d9b69d5791271d75d41679fdfa990d49fd82ea
Jordan Lee <jordan@jordanlee.net>
2024-03-06 22:22:02 +1100

    update webapi component to display weather forecast in table

--- spa/src/app/webapi/webapi.component.ts
+++ spa/src/app/webapi/webapi.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { protectedResources } from '../auth-config';
+
+type WeatherForecast = {
+  date: Date;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string | null;
+}
+
+type WeatherForecastResult = {
+  result: WeatherForecast[]
+};
 
 @Component({
   selector: 'app-webapi',
   templateUrl: './webapi.component.html',
-  styleUrl: './webapi.component.css'
+  styleUrls: ['./webapi.component.css']
 })
-export class WebapiComponent {
+export class WebapiComponent implements OnInit {
+  todoListEndpoint: string = protectedResources.api.endpoint;
+  weatherForecast: WeatherForecastResult = {
+    result: []
+  };
 
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.getProfile();
+  }
+
+  getProfile() {
+    this.http.get<WeatherForecastResult>(this.todoListEndpoint)
+      .subscribe((profile) => {
+        this.weatherForecast = profile;
+      });
+  }
 }
