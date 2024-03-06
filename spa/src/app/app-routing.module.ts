289a9eb644e79e1ce3ace83d6f846f9dcf4c3d92
Jordan Lee <jordan@jordanlee.net>
2024-03-06 22:12:37 +1100

    add routes

--- spa/src/app/app-routing.module.ts
+++ spa/src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { WebapiComponent } from './webapi/webapi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* Changes start here. */
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    // The profile component is protected with MSAL Guard.
+    canActivate: [MsalGuard]
+  },
+  {
+    path: 'webapi',
+    component: WebapiComponent,
+    // The profile component is protected with MSAL Guard.
+    canActivate: [MsalGuard]
+  },
+  {
+    // The home component allows anonymous access
+    path: '',
+    component: HomeComponent
+  }
+  /* Changes end here. */
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  /* Changes start here. */
+  // Replace the following line with the next one
+  //imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation:'enabledBlocking'
+  })],
+  /* Changes end here. */
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
